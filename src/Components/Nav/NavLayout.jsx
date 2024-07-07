cebb466aa3fd8b08538a945bd71696483004fc62
EndrXOriginial <chris.pin@hotmail.ca>
2024-07-06 22:35:36 -0400

    finished style for nav and footer

--- src/Components/Nav/NavLayout.jsx
+++ src/Components/Nav/NavLayout.jsx
@@ -1,22 +1,23 @@
 import { NavLink, Outlet } from 'react-router-dom';
+import logo from '../../assets/images/logo.svg';
 import './nav.css';
 
 export const NavLayout = () => {
     return (
         <>
         <nav className='nav'>
-            <div className='wrapper-desktop'>
-                {/* <img src='/assets/images/logo.svg' alt='Logo of the company' /> */}
+            <div className='wrapper-desktop nav-div'>
+                <img src={logo} alt='Logo of the company' />
                 <ul className='nav-ul'>
-                    <li className='nav-li'><NavLink to='/'>Home</NavLink></li>
-                    <li className='nav-li'><NavLink to='/home'>Products</NavLink></li>
-                    <li className='nav-li'><NavLink to='/home'>About</NavLink></li>
+                    <li className='nav-li'><NavLink className={({isActive}) => 'nav-link' + (isActive ? ' nav-link-active' : '')} to='/'>Home</NavLink></li>
+                    <li className='nav-li'><NavLink className={({isActive}) => 'nav-link' + (isActive ? ' nav-link-active' : '')} to='/home'>Products</NavLink></li>
+                    <li className='nav-li'><NavLink className={({isActive}) => 'nav-link' + (isActive ? ' nav-link-active' : '')} to='/home'>About</NavLink></li>
                 </ul>
             </div>
         </nav>
         <Outlet />
         <footer className='footer'>
-            <div className='wrapper-desktop'>
+            <div className='wrapper-desktop center'>
                 <p className='footer-p'>Company is fake and made with FakeStoreApi</p>
             </div>
         </footer>
