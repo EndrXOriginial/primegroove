219d819562cea76e3a722322f0b9dd92d0345b9b
EndrXOriginial <chris.pin@hotmail.ca>
2024-07-10 11:24:03 -0400

    added animation to the menu button

--- src/Components/Nav/NavLayout.jsx
+++ src/Components/Nav/NavLayout.jsx
@@ -5,19 +5,31 @@ import userIcon from '../../assets/icon/user.svg';
 import './navLayout.css';
 
 export const NavLayout = () => {
-    const handleClick = () => {
+    const handleClick = (e) => {
+        let target = e.target;
         const modale = document.querySelector('.nav-modale');
         modale.classList.toggle("visible");
-        
+        if (target.childElementCount === 0) {
+            target = target.parentElement;
+        }
+        for (let i = 0; i < target.childElementCount; i++) {
+            if (target.childNodes[i].classList.contains('menu-close-anim')) {
+                target.childNodes[i].classList.toggle('menu-close-anim');
+                target.childNodes[i].classList.toggle('menu-open-anim');
+            } else {
+                target.childNodes[i].classList.toggle('menu-open-anim');
+                target.childNodes[i].classList.toggle('menu-close-anim');
+            }
+        }
     }
 
     return (
         <>
         <div className="nav-burger-menu">
             <svg onClick={handleClick} viewBox="0 0 100 100" className='menu-svg'>
-                <line y1="35" x1="20" x2="80" y2="35" stroke="#56514A" stroke-width="3"/>
-                <line y1="50" x1="20" x2="80" y2="50" stroke="#56514A" stroke-width="3"/>
-                <line y1="65" x1="20" x2="80" y2="65" stroke="#56514A" stroke-width="3"/>
+                <line className='menu-open-anim' y1="35" x1="20" x2="80" y2="35" stroke="#56514A" stroke-width="3"/>
+                <line className='menu-open-anim' y1="50" x1="20" x2="80" y2="50" stroke="#56514A" stroke-width="3"/>
+                <line className='menu-open-anim' y1="65" x1="20" x2="80" y2="65" stroke="#56514A" stroke-width="3"/>
             </svg>
         </div>
         <div className="nav-modale">
