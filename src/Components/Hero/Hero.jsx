4c4553a00a8890c6e7a7d23c0cc59464030413d3
EndrXOriginial <chris.pin@hotmail.ca>
2024-07-10 13:19:21 -0400

    the carrousel sends current id back to parent for link

--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -6,11 +6,21 @@ import 'swiper/css';
 import 'swiper/css/pagination';
 import 'swiper/css/autoplay';
 import './hero.css';
+import { Link } from "react-router-dom";
 
 
 export const Hero = () => {
     const [data, setData] = useState([]);
+    const [currentId, setCurrentId] = useState(0)
     const [isLoading, setIsLoading] = useState(true);
+
+    const handleChildData = (id) => {
+        setCurrentId(id);
+    };
+
+    const testId = () => {
+        console.log(currentId);
+    }
 
     useEffect(() => {
         const fetchData = async () => {
@@ -34,26 +44,42 @@ export const Hero = () => {
     return (
         <>
         <section className="hero">
-            <div className="hero-content">Don't forget, mobile first!!!!!!</div>
-            <div className="hero-media"><Carrousel products={data} /></div>
+            <div className="hero-content">
+                <div className="hero-content-top">
+                    <h1 className="hero-title">Starting school with the right equipement</h1>
+                    <h2 className="hero-subtitle">Get your child ready for the upcoming school year with these limited discounts.</h2>
+                    </div>
+                <div className="hero-content-bottom">
+                    <h2 className="hero-bottom-price"><span>99.99$</span>69.99$</h2>
+                    <button type="button" className="hero-button">Add to Cart</button>
+                    <Link to={`/product/${currentId}`}>See The Items</Link>
+                </div>
+            </div>
+            <div className="hero-media"><Carrousel products={data} sendIdParent={handleChildData} /></div>
         </section>
         </>
     )
 }
 
-const Carrousel = ({products}) => {
+const Carrousel = ({products, sendIdParent}) => {
+    const [prodId, setProdId] = useState();
+
+    const onHandleChange = (e) => {
+        sendIdParent(products[e.activeIndex].id);
+    }
+
     if (products.length == 0) {
         console.log('Nothing in the Carrousel');
         return <div>loading...</div>
-    } else if (products.length == 3) {
-        console.log('we have smt boys');
     }
 
     return (
         <>
-        <Swiper slidesPerView={1} modules={[Pagination, Autoplay]} pagination={{clickable: true}}>
+        <Swiper className="mySwiper" slidesPerView={1} modules={[Pagination, Autoplay]} pagination={{clickable: true}} onSlideChange={onHandleChange} onSwiper={swiper => sendIdParent(products[swiper.activeIndex].id)}>
             {products.map(product => {
-                return <SwiperSlide key={product.id}><img src={product.image} alt="hello" className="swiper-img" /></SwiperSlide>
+                return <SwiperSlide key={product.id} id={product.id}>
+                            <img src={product.image} alt="hello" className="swiper-img" />
+                    </SwiperSlide>
             })}
         </Swiper>
         </>
